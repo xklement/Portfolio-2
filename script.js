2311c0a633d417d2bdd6319c473a00845e223427
Clement Xu <clement.xu@epitech.eu>
2023-12-31 14:49:22 +0100

    Init portfolio

---
+++ script.js
@@ -0,0 +1,54 @@
+const carouselText = [
+  { text: "Full-stack developer.", color: "orange" },
+  { text: "Mobile developer.", color: "yellow" },
+];
+
+async function typeSentence(sentence, eleRef, delay = 100) {
+  const letters = sentence.split("");
+  let i = 0;
+  while (i < letters.length) {
+    await waitForMs(delay);
+    $(eleRef).append(letters[i]);
+    i++;
+  }
+  return;
+}
+
+async function deleteSentence(eleRef) {
+  const sentence = $(eleRef).html();
+  const letters = sentence.split("");
+  let i = 0;
+  while (letters.length > 0) {
+    await waitForMs(100);
+    letters.pop();
+    $(eleRef).html(letters.join(""));
+  }
+}
+
+function waitForMs(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function carousel(carouselList, eleRef) {
+  var i = 0;
+  while (true) {
+    console.log("here");
+    updateFontColor(eleRef, carouselList[i].color);
+    await typeSentence(carouselList[i].text, eleRef);
+    await waitForMs(1500);
+    await deleteSentence(eleRef);
+    await waitForMs(500);
+    i++;
+    if (i >= carouselList.length) {
+      i = 0;
+    }
+  }
+}
+
+function updateFontColor(eleRef, color) {
+  $(eleRef).css("color", color);
+}
+
+carousel(carouselText, "#sentence");
+
+
